3465f5f40f426710eb7519a103fff9f2060a146d
unknown <brandonpu2020@gmail.com>
2024-08-07 21:55:27 -0600

    Crud Clientes Completo

--- src/main/java/com/brandonpu/webapp/biblioteca/controller/ClienteController.java
+++ src/main/java/com/brandonpu/webapp/biblioteca/controller/ClienteController.java
@@ -7,8 +7,11 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,6 +32,15 @@ public class ClienteController {
         return clienteService.listarClientes();
     }
 
+    @GetMapping("/{dpi}")
+    public ResponseEntity<Cliente> buscarClientePorDpi(@PathVariable Long dpi){
+        try {
+            return ResponseEntity.ok(clienteService.buscarClientePorDpi(dpi));
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(null);
+        }
+    }
+
     @PostMapping("/")
     public ResponseEntity<Map<String, String>> agregarCliente(@RequestBody Cliente cliente){
         Map<String, String> response = new HashMap<>();
@@ -41,6 +53,38 @@ public class ClienteController {
             return ResponseEntity.badRequest().body(response);
         }
 
+    }
+
+    @PutMapping("/{dpi}")
+    public ResponseEntity<Map<String, String>> editarCliente(@PathVariable Long dpi, @RequestBody Cliente clienteNuevo){
+        Map<String, String> response = new HashMap<>();
+        try {
+            Cliente cliente = clienteService.buscarClientePorDpi(dpi);
+            cliente.setDpi(clienteNuevo.getDpi());
+            cliente.setNombre(clienteNuevo.getNombre());
+            cliente.setApellido(clienteNuevo.getApellido());
+            cliente.setTelefono(clienteNuevo.getTelefono());
+            clienteService.guardarCliente(cliente);
+            response.put("message", "El cliente se ha editado con exito");
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            response.put("err", "El cliente no se pudo editar");
+            return ResponseEntity.badRequest().body(response);
+        }
+    }
+
+    @DeleteMapping("/{dpi}")
+    public ResponseEntity<Map<String, String>> eliminarCliente(@PathVariable Long dpi){
+        Map<String, String> response = new HashMap<>();
+        try {
+            Cliente cliente = clienteService.buscarClientePorDpi(dpi);
+            clienteService.eliminarCliente(cliente);
+            response.put("message", "Cliente eliminado con exito");
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            response.put("err", "El cliente no se pudo eliminar");
+            return ResponseEntity.badRequest().body(response);
+        }
     }
 
 }
--- src/main/java/com/brandonpu/webapp/biblioteca/model/Cliente.java
+++ src/main/java/com/brandonpu/webapp/biblioteca/model/Cliente.java
@@ -3,6 +3,7 @@ package com.brandonpu.webapp.biblioteca.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.ToString;
 
@@ -13,9 +14,13 @@ import lombok.ToString;
 public class Cliente {
     
     @Id
+    @NotNull(message = "El Dpi no puede estar vacio")
     private Long dpi;
+    @NotNull(message = "El Nombre del cliente no puede ser nulo")
     private String nombre;
+    @NotNull(message = "El Apellido del cliente no puede ser nulo")
     private String apellido;
+    @NotNull(message = "El Telefono del cliente no puede ser nulo")
     private String telefono;
     
 }
